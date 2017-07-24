4d6b845c5afd6676cc2b8fc05ff06393f9502999
Fabrizzo <fre-osa@mail.ru>
2017-07-24 16:07:37 +0700

    Cpp new programms learning

---
+++ Generalizer Class/Generalizer Class/Generalizer Class.cpp
@@ -0,0 +1,34 @@
+// Generalizer Class.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+template<class A, class B> class MyClass {
+public: 
+	A first;
+	B second;
+	MyClass(A f, B s) {
+		first = f;
+		second = s;
+	}
+	void show() {
+		cout << "Первое поле " << first << endl;
+		cout << "Второе поле " << second << endl;
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	MyClass<int, char> objA(100, 'A');
+	MyClass<string, double> objB("текст", 10.5);
+	MyClass<char*, string> objC("первый", "второй");
+	MyClass<int, int> objD(1, 2);
+	objA.show();
+	objB.show();
+	objC.show();
+	system("pause>nul");
+    return 0;
+}
+
